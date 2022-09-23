1beda11dd5d29c727ba975a40214d3981bc272a3
judenfors <rudenfors.eric@gmail.com>
2022-09-23 11:10:11 +0200

    Add project files.

---
+++ uppgift3._13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    namespace Uppgift3._13
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hur gammal är du, svara i hela årtal");
+
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får vara med i tävlingen");
+            }
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du är för ung för att vara med i tävlingen");
+            }
+
+            if(ålder > 19)
+            {
+                Console.WriteLine("Du är för gammal för att vara med i tävlingen");
+            }
+        }
+    }
+}
+
